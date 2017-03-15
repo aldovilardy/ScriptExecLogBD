c72c836e88c0b11d6ac179e31f9880e7e7f8632e
Aldo Fernando Vilardy Roa <aldovilardy@gmail.com>
2017-03-15 09:14:41 -0500

    correción error ortografico

--- exec_log.bat
+++ exec_log.bat
@@ -4,7 +4,7 @@ echo. Autor: Aldo Vilardy 30/02/2015
 echo. La bitacora de ejecucion queda regitrada en el fichero info.txt
 if exist info.txt ( 
 erase info.txt)
-for %%G in (*.sql) do (sqlcmd /S 127.0.0.1 /d NembreBaseDatos -E -i"%%G" -b -o msg.txt
+for %%G in (*.sql) do (sqlcmd /S 127.0.0.1 /d NombreBaseDatos -E -i"%%G" -b -o msg.txt
 if errorlevel 1 (
 echo. >> info.txt
 echo.%date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,8% Error en script %%G >> info.txt
